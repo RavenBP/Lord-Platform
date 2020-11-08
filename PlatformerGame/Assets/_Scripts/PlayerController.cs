8014f3c2dcb50fed2da357533dd2b9af2f305220
RavenBP <ravenbpowless@gmail.com>
2020-11-08 12:31:56 -0500

    Updated PlayerController

--- PlatformerGame/Assets/_Scripts/PlayerController.cs
+++ PlatformerGame/Assets/_Scripts/PlayerController.cs
@@ -8,41 +8,80 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     Rigidbody2D rigidbody2D;
     [SerializeField]
+    SpriteRenderer spriteRenderer;
+    [SerializeField]
     float speed = 5.0f;
 
     float xSeperation = 0.3f;
 
-    Vector2 movementVector;
     Vector2 screenTouchPosition;
+
+    // Jumping
+    public float jumpForce;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask ground;
+
+    private int extraJumps;
+    public int extraJumpsValue;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        extraJumps = extraJumpsValue;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpsValue; // Reset jumps
+        }
+
+        screenTouchPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        if (Input.touchCount > 0)
+        {
+            if (screenTouchPosition.x > 0.6f && extraJumps > 0) // Player is attempting to jump in air
+            {
+                rigidbody2D.velocity = Vector2.up * jumpForce;
+                extraJumps--;
+            }
+            else if (screenTouchPosition.x > 0.6f && extraJumps == 0 && isGrounded == true) // Player is attempting to jump
+            {
+                rigidbody2D.velocity = Vector2.up * jumpForce;
+            }
+        }
+    }
+
+    void FixedUpdate()
     {
         Move();
     }
 
     void Move()
     {
-        screenTouchPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, ground); // Player is on "Ground" Layer
 
         if (Input.touchCount > 0)
         {
-            if (screenTouchPosition.x > xSeperation)
+            if (screenTouchPosition.x > xSeperation && screenTouchPosition.x < 0.6f) // Player attempting to move right
             {
-                movementVector = new Vector2(1.0f, 0.0f) * speed * Time.deltaTime;
-                rigidbody2D.transform.Translate(movementVector);
+                rigidbody2D.velocity = new Vector2(1.0f * speed, rigidbody2D.velocity.y);
+                spriteRenderer.flipX = false;
             }
-            else if (screenTouchPosition.x < xSeperation)
+            else if (screenTouchPosition.x < xSeperation) // Player attempting to move left
             {
-                movementVector = new Vector2(1.0f, 0.0f) * -speed * Time.deltaTime;
-                rigidbody2D.transform.Translate(movementVector);
+                rigidbody2D.velocity = new Vector2(-1.0f * speed, rigidbody2D.velocity.y);
+                spriteRenderer.flipX = true;
             }
+        }
+        else // Player does not want to move
+        {
+            rigidbody2D.velocity = new Vector2(0.0f, rigidbody2D.velocity.y); // Stop velocity on x axis
         }
     }
 }
