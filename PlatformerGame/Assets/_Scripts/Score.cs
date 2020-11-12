4a7a01014ecec43db31fe7eadbee31403d9a99b0
RavenBP <ravenbpowless@gmail.com>
2020-11-12 18:15:21 -0500

    Added Score script

---
+++ PlatformerGame/Assets/_Scripts/Score.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField]
+    Text scoreText;
+
+    static int scoreVal = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "GameScene")
+        {
+            scoreVal = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + scoreVal;
+    }
+
+    public int GetScore()
+    {
+        return scoreVal;
+    }
+}
